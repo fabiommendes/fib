509dcc0977d85ce47ca55f8e81661f707c8458d2
Gustavo Zorzeto <89036888+gzorzeto@users.noreply.github.com>
2021-08-16 15:39:17 -0300

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,9 +1,9 @@
 # Calcula a sequência de Fibonacci (sqn)
 n = int(input("n: "))
 x, y = 1, 1
-
+fib =[]
 for _ in range(n):
-    print(x)
-    x = y
-    y = x + y
+    fib.append(x)
+    x , y = y, x + y
+print (fib)    
     
